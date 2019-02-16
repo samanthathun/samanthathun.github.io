04e3d382a0213fbbe033ead6fa74ef6beb34b104
samanthathun <bev04033@byui.edu>
2019-02-15 18:12:54 -0700

    update

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,3 +1,14 @@
-var date = newDate();
+var day = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
-document.getElementById(date).innerHTML=date;
+var n = weekday[day.getDay()];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+document.getElementById("demo").innerHTML = n + " " + months[day.getMonth()] + " " + day.getDate() + ", " + day.getFullYear();
