32bc065abe9d5f16396cfe1b5d73a00d1a0a2f79
samanthathun <bev04033@byui.edu>
2019-02-21 20:35:39 -0700

    update

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,13 +1,6 @@
-function doInputOutput() {
-    let temp = parseFloat(document.getElementById("temp").value);
-    let wind = parseFloat(document.getElementById("wind").value);
-    let result = windChill(temp, wind);
 
-document.getElementById ("outputDiv").innerHTML = result;
-}
+    let temp = parseFloat(document.getElementById("temp").innerText);
+    let wind = parseFloat(document.getElementById("wind").innerText);
+    let result = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind,0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
 
-function windChill(tempF, speed) {
-  var f = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed,0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
-   return f;
-
-}
+document.getElementById ("outputDiv").innerHTML = result.toFixed(1);
