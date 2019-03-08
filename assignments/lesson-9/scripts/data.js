a272266e4e978e6ddd8e2acf5fd37d777d561b4d
samanthathun <bev04033@byui.edu>
2019-03-08 14:13:31 -0700

    update

--- assignments/lesson-9/scripts/data.js
+++ assignments/lesson-9/scripts/data.js
@@ -10,36 +10,37 @@ request.send();
 request.onload = function () {
     let towndata = request.response;
 
-    console.log(towndata);
+   
 
     let town = towndata['towns'];
-
+    console.log(town);
     for (let i = 0; i < town.length; i++) {
-        console.log(town.name);
-        if (town.name == "Preston" || town.name == "Soda Springs" || town.name == "Fish Haven" ) {
-            let divtown =document.createElement('div.townbox');
-            let myH2 = document.createElement('h2');
+    
+        if (town[i].name == "Preston" || town[i].name == "Soda Springs" || town[i].name == "Fish Haven" ) {
+            let divtown =document.createElement('div');
+            divtown.setAttribute('class', 'townbox');
             let myH3 = document.createElement('h3');
+            let myH4 = document.createElement('h4');
             let myP1 = document.createElement('p');
             let myP2 = document.createElement('p');
             let myP3 = document.createElement('p');
+            let prestonimg = ;
+            let sodaspringsimg = ;
+            let fishhavenimg = ;
 
+            myH3.textContent = town[i].name;
+            myH4.textContent = town[i].motto;
+            myP1.textContent = "Year Founded: " + town[i].yearFounded;
+            myP2.textContent = "Current Population: " + town[i].currentPopulation;
+            myP3.textContent = "Average Rainfall: " + town[i].averageRainfall;
 
-            myH2.textContent = town.name;
-            myH3.textContent = town.motto;
-            myP1.textContent = town.yearFounded;
-            myP2.textContent = town.currentPopulation;
-            myP3.textContent = town.averageRainFall;
-
-
-            divtown.appendChild(myH2);
             divtown.appendChild(myH3);
+            divtown.appendChild(myH4);
             divtown.appendChild(myP1);
             divtown.appendChild(myP2);
             divtown.appendChild(myP3);
-            output.appendChild(divtown);
 
-    
+            output.appendChild(divtown);
         }
     }
 }
