467114f593563137c5473ab9e003d34dfc384079
samanthathun <bev04033@byui.edu>
2019-03-14 19:27:55 -0600

    updateweatherapi

--- assignments/lesson-10/scripts/forecastapi.js
+++ assignments/lesson-10/scripts/forecastapi.js
@@ -1,16 +1,16 @@
 let forecastRequest = new XMLHttpRequest();
-let apiURLstring = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=b3f937ac836b548fdae15d52ba76d87b";
+let apiURLstring = "api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=b3f937ac836b548fdae15d52ba76d87b";
 forecastRequest.open("Get", apiURLstring, true);
 forecastRequest.send();
 
 forecastRequest.onload = function() {
 
-    let weatherData = JSON.parse(forecastRequest.responseText);
+    let forecastData = JSON.parse(forecastRequest.responseText);
 //console.log(weatherData);
 document.getElementById("current-temp").innerHTML=weatherData.main.temp;
 
-let icon = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
-let desc = "weatherData.weather[0].descripion";
+let icon = "http://openweathermap.org/img/w/" + forecastData.weather[0].icon + ".png";
+let desc = "forecastData.weather[0].descripion";
 
 document.getElementById("cc-img").setAttribute('src', icon);
 document.getElementById("cc-img").setAttribute('alt', desc);
