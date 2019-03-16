d81521aa20f2a3dab4b204e9dc864bccaf9a5111
samanthathun <bev04033@byui.edu>
2019-03-16 10:23:56 -0600

    update

--- assignments/lesson-10/scripts/forecastapi.js
+++ assignments/lesson-10/scripts/forecastapi.js
@@ -6,7 +6,27 @@ forecastRequest.send();
 forecastRequest.onload = function () {
 
     let forecastData = JSON.parse(forecastRequest.responseText);
+
+
     console.log(forecastData);
+
+
+
+    var day = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var n = weekday[day.getDay() + 1];
+
+    document.getElementById("forecastday").innerHTML = n;
+
+
 
     let hightempforecast = [];
     let counter = 0;
@@ -17,32 +37,39 @@ forecastRequest.onload = function () {
             hightempforecast[counter] = forecastData.list[i].main.temp;
 
             counter++;
+           
+            }
+
+            for (let i = 1; i <= 5; i++) {
+
+                document.getElementById('current-temp' + i).innerHTML = hightempforecast[i - 1];
+
+
+
+            }
 
         }
 
-        for (let i = 1; i <= 5; i++) {
+        let icon = "http://openweathermap.org/img/w/10d.png";
+        let desc = "forecastData.weather.description";
 
-            document.getElementById('current-temp' + i).innerHTML = hightempforecast[i - 1];
+
+        for (let i = 0; i < forecastdata.list.length; i++) {
+            if (forecastData.list[i].dt_txt.match(/18:00:00/)) {
+                hightempforecast[counter] = icon;
+
+                counter++
+            }
+            for (let i = 1; i <= 5; i++) {
+
+                document.getElementById("cc-img").setAttribute('src', icon);
+                document.getElementById("cc-img").setAttribute('alt', desc);
+
+                document.getElementById('cc-img' + i).innerHTML = icon[i - 1];
+
+            }
+
 
         }
 
     }
-
-
-
-    let icon = "http://openweathermap.org/img/w/" + forecastData.weather[0].icon + ".png";
-    let desc = "forecastData.weather[0].description";
-
-
-
-    document.getElementById("cc-img").setAttribute('src', icon);
-    document.getElementById("cc-img").setAttribute('alt', desc);
-
-
-
-
-
-
-}
-
-
